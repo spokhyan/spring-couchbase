f7fe3d65059cd3c1eadf5ad1071a99eac9a4dbd7
spokhyan <erve#@pg1h>
2020-08-15 12:51:52 -0400

    initial commit after creating spring boot files for project using spring.io(intellij spring assistance).

---
+++ src/main/java/com/skp/noSQL/SpringCouchbaseApplication.java
@@ -0,0 +1,13 @@
+package com.skp.noSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCouchbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCouchbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skp/noSQL/SpringCouchbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skp.noSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCouchbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
